2d498b6945b2df4d67677c003743acc5a147b82a
jonniek01 <jontejonhn@gmail.com>
2022-02-13 14:08:33 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route,Routes,Link } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Home  from './pages/Home';
+import { About }  from './pages/About';
+import Profile from './pages/Profile';
+import NotFound from './pages/NotFound';
+import Post from './pages/Post';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter >
+        <div className="App">
+      <Header/>
+      <ul className='nav'>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/post">Post</Link>
+        </li>
+
+
+
+
+
+      </ul>
     </div>
+    <Routes>
+    <Route path='/' element={<Home/>} exact/>
+    <Route path='/about' element={<About/>} exact/>
+    <Route path='/profile' element={<Profile/>} exact/>
+    <Route path='/post/:id' element={<Post/>} exact/>
+
+    <Route path="*" element={<NotFound/>} exact/>
+    <div>post</div>
+
+
+
+
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import { useEffect } from 'react'
+function Home() {
+    useEffect(() => {
+      
+    
+      window.scroll(0,0)
+    }, [])
+    
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
