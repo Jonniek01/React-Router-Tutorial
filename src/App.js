9b3677add7dc6b79b0ca14ce854aa77b036383f4
jonniek01 <jontejonhn@gmail.com>
2022-02-13 15:34:36 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import { BrowserRouter, Route,Routes,Link } from 'react-router-dom';
+import { BrowserRouter, Route,Routes,Link, Navigate  } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Home  from './pages/Home';
 import { About }  from './pages/About';
 import Profile from './pages/Profile';
 import NotFound from './pages/NotFound';
-import Post from './pages/Post';
+import { useState } from 'react';
 
 function App() {
+  const [login,setLogin]=useState(false)
   return (
     <BrowserRouter >
         <div className="App">
       <Header/>
+
       <ul className='nav'>
         <li>
           <Link to="/">Home</Link>
@@ -23,21 +25,22 @@ function App() {
         <li>
           <Link to="/profile">Profile</Link>
         </li>
-        <li>
-          <Link to="/post">Profile</Link>
-        </li>
 
 
 
 
 
       </ul>
+      <button onClick={()=>setLogin(!login)}>{login?"logout":"login"}</button>
+
     </div>
     <Routes>
     <Route path='/' element={<Home/>} exact/>
     <Route path='/about' element={<About/>} exact/>
-    <Route path='/profile' element={<Profile/>} exact/>
-    <Route path='/post' element={<Post/>} exact/>
+
+    <Route path='/profile' element= {login?<Profile/>:<Navigate replace to="/"></Navigate>} />
+
+
 
     <Route path="*" element={<NotFound/>} exact/>
 
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Routes, Route,Link,useMatch } from 'react-router-dom'
+import ViewProfile from '../components/ViewProfile'
+import { EditProfile } from '../components/EditProfile'
+
 
 export const Profile = () => {
+    const {path,url}=useMatch();
   return (
-    <div>Profile</div>
+   <>
+   <h>profile</h>
+   <ul>
+       <li>
+           <Link to={`${url}/profile/viewprofile`}>VIEW</Link>
+       </li>
+       <li>
+           <Link to={`${url}/profile/editprofile`}>EDIT</Link>
+       </li>
+
+   </ul>
+   <Routes>
+       <Route path={`${path}/viewprofile`} element={ViewProfile}/>
+       <Route path={`${path}/editrofile`} element={EditProfile}/>
+
+   </Routes>
+   </>
   )
 }
 
